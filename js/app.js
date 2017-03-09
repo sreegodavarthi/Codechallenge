ddd05e1c37d324cce8b5fff605272786f10c4f74
Sree Godavarthi <Sree Godavarthi>
2017-03-09 10:01:07 -0600

    Resolved issues according to the code challenge

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var myApp = angular.module("myApp",[]);
 
 myApp.controller("myCtrl", function ctrl($scope) {
-    $scope.things =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 112, 113];
+    $scope.numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 112, 113];
     $scope.filterEvenStartFrom = function (index) {
         return function (item) {
             return index++ % 2 == 1;
